8ebb07ecddc8185f549454fad6007c89574ed6ac
Irina2304 <110673165+Irina2304@users.noreply.github.com>
2023-10-01 08:28:51 +0200

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,7 +3,7 @@ import styles from './Statistics.module.css'
 export const Statistics = ({title, stats}) => {
   return(
     <section className={styles.statistics}>
-      <h2 className={styles.title}> {title}</h2>
+      {title && <h2 className={styles.title}> {title}</h2>}
       <ul className={styles.statList}>
         {stats.map(data => {
           return (
